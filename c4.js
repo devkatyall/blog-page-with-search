76a2e0390fa61c5cf51384bb20f863312bec9328
devkatyall <156537171+devkatyall@users.noreply.github.com>
2024-03-16 21:57:42 -0400

    Update c4.js

--- c4.js
+++ c4.js
@@ -3,15 +3,9 @@ const blogSection = document.querySelector(".blog")
 const inputValue = document.querySelector("#input")
 let post =[];
 
-inputValue.addEventListener("input",(search)=>{
-    const input = search.target.value.toLowerCase()
-    post.forEach(
-        (user)=>{
-            const isTrue = user.title.includes(input) || user.about.includes(input)
-            user.element.classList.toggle("hide", !isTrue)
-        }
-    )
-})
+function highlightText(element, query) {
+    element.innerHTML = element.textContent.replace(query, `<span class="highlight"> ${query}</span>`);
+}
 
 fetch("https://jsonplaceholder.typicode.com/posts")
     .then(dost=>dost.json())
@@ -25,4 +19,23 @@ fetch("https://jsonplaceholder.typicode.com/posts")
             blogSection.append(card)
             return {title: user.title, about: user.body, element: card}
         });
+})
+
+inputValue.addEventListener("input",(search)=>{
+    const input = search.target.value.toLowerCase()
+    post.forEach(
+        (user)=>{
+            const isTrue = user.title.includes(input) || user.about.includes(input)
+            user.element.classList.toggle("hide", !isTrue)
+            const titleElement = user.element.querySelector(".title");
+            const descriptionElement = user.element.querySelector(".about");
+            if (isTrue) {
+                highlightText(titleElement, input);
+                highlightText(descriptionElement, input);
+            } else {
+                titleElement.innerHTML = user.title;
+                descriptionElement.innerHTML = user.about;
+            }
+        }
+    )
 })
